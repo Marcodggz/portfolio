ac99513919e03b88ed59ec80c1a8c9dd4e5871fd
Marco Dominguez <210174564+Marcodggz@users.noreply.github.com>
2026-08-11 13:13:17 +0200

    content(courses): add React course

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -3,6 +3,7 @@ import type { Certification } from '../types'
 export const certifications: Certification[] = [
   { name: 'TypeScript', issuer: 'Codecademy' },
   { name: 'JavaScript', issuer: 'Codecademy' },
+  { name: 'React', issuer: 'Codecademy' },
   { name: 'Node.js', issuer: 'Codecademy' },
   { name: 'Learn Python 3', issuer: 'Codecademy' },
   { name: 'Introduction to PHP', issuer: 'Codecademy' },
